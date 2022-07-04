ac9bc2d2fc684954aba416292650cf0c260203d8
mensahTribeWeb <ndm.acct85@gmailcom>
2022-07-03 22:55:58 -0500

    joins update

--- 03_Joins/exercise/Exercises/03_actors_last_name_jones.sql
+++ 03_Joins/exercise/Exercises/03_actors_last_name_jones.sql
@@ -1,3 +1,19 @@
--- 3. For all actors with the last name of "Jones", display the actor's name and movie titles they appeared in. Order the results by the actor names (A-Z). (48 rows)
+-- 3. For all actors with the last name of "Jones", display the
+--actor's name and movie titles they appeared in. Order the results 
+--by the actor names (A-Z). (48 rows)
+
+SELECT title, person_name
+FROM person AS p
+
+JOIN movie_actor
+ON movie_actor.actor_id = p.person_id
+JOIN movie AS m
+ON m.movie_id = movie_actor.movie_id
+WHERE person_name LIKE '% Jones'
+
+ORDER BY person_name ASC;
+
+
+
 
 
