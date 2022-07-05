be8de78cfad8176aac820d15bc13575510c6affc
mensahTribeWeb <ndm.acct85@gmailcom>
2022-07-04 19:54:07 -0500

    INSERT UPDATE DELETE COMPLETE

--- 04_INSERT_UPDATE_and_DELETE/exercise/Exercises/02_add_movie.sql
+++ 04_INSERT_UPDATE_and_DELETE/exercise/Exercises/02_add_movie.sql
@@ -7,3 +7,7 @@
 INSERT INTO movie (title, overview, release_date, length_minutes)
 VALUES ('Euclidean Pi', 'The epic story of Euclid as a pizza delivery
 boy in ancient Greece', '2015-03-14','194');
+
+UPDATE movie 
+SET release_date = '3/14/2015'
+WHERE title = 'Euclidean Pi';
