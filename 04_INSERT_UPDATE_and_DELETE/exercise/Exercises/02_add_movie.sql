158cdaa3f6c03c229fd71ea7f57eb772f095aa12
mensahTribeWeb <ndm.acct85@gmailcom>
2022-07-04 20:03:35 -0500

    INSERT UPDATE DELETE COMPLETE

--- 04_INSERT_UPDATE_and_DELETE/exercise/Exercises/02_add_movie.sql
+++ 04_INSERT_UPDATE_and_DELETE/exercise/Exercises/02_add_movie.sql
@@ -1,6 +1,5 @@
 -- 2. Add "Euclidean Pi" to the movie table. 
---The overview is "The epic story of Euclid as a pizza delivery
---boy in ancient Greece" and was released 3/14/2015. 
+--The overview is "The epic story of Euclid as a pizza delivery boy in ancient Greece" and was released 3/14/2015. 
 --Since its an epic, the run length is 3 hours and 14 minutes 
 --(194 minutes). (1 row)
 
@@ -11,3 +10,7 @@ boy in ancient Greece', '2015-03-14','194');
 -- UPDATE movie 
 -- SET release_date = '3/14/2015'
 -- WHERE title = 'Euclidean Pi';
+
+UPDATE movie 
+SET overview = 'The epic story of Euclid as a pizza delivery boy in ancient Greece'
+WHERE title = 'Euclidean Pi';
